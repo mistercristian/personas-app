410c9997cc2d6058acf98455128054017339dbdc
Cristian Guzman <cristianguzbo@gmail.com>
2024-09-29 15:58:02 -0500

    Version comunas List

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\ComunaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/comunas',[ComunaController::class, 'index']);
